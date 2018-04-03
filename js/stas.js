c9bbcace58779785b8b3613e399d1a96d3329f8e
ratelChief <dima1002010@live.com>
2018-04-03 15:35:04 +0300

    module2-task1

--- js/stas.js
+++ js/stas.js
@@ -9,7 +9,7 @@ var FONT_GAP = 16;
 var BAR_WIDTH = 40;
 var barHeight = -150;
 var PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
-var otherPlayersColor = 'rgba(0, 0, 255, ' + Math.random() + ')';
+
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -46,6 +46,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP * 2);
 
   for (var i = 0; i < names.length; i++) {
+    var otherPlayersColor = 'rgba(0, 0, 255, ' + Math.random() + ')';
     ctx.fillStyle = '#000';
     ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP + FONT_GAP + (GAP + BAR_WIDTH * 2) * i, CLOUD_HEIGHT - GAP - FONT_GAP - Math.floor((-barHeight * times[i]) / maxTime) - FONT_GAP);
     ctx.fillText(names[i], CLOUD_X + GAP + FONT_GAP + (GAP + BAR_WIDTH * 2) * i, CLOUD_HEIGHT - GAP - GAP);
