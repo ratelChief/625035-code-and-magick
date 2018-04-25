5311cda2adfdcfe18a085e0c6b16c6f4f4cfdf60
ratelChief <dima1002010@live.com>
2018-04-25 11:30:46 +0300

    troubleShooting

---
+++ js/dialog.js
@@ -0,0 +1,117 @@
+'use strict';
+(function () {
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var inputIsFocused = false;
+  var userNameInput = setup.querySelector('.setup-user-name');
+
+  var onPopupEscPress = function (evt) {
+    if (inputIsFocused) {
+      evt.stopPropagation();
+    } else {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  var initialStateOfPopup = function () {
+    var setupState = setup.getBoundingClientRect();
+    var initialCoords = {
+      x: setupState.left,
+      y: setupState.top
+    };
+    setup.dataset.xCoord = initialCoords.x;
+    setup.dataset.yCoord = initialCoords.y;
+    console.log(initialCoords);
+    console.log(setup.dataset.yCoord);
+    console.log(setup.dataset.xCoord);
+  };
+
+  userNameInput.addEventListener('focus', function () {
+    inputIsFocused = true;
+  });
+
+  userNameInput.addEventListener('blur', function () {
+    inputIsFocused = false;
+  });
+
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+    initialStateOfPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+    initialStateOfPopup();
+  });
+
+  setupClose.addEventListener('click', function () {
+    setup.style.left = setup.dataset.xCoord + 'px';
+    setup.style.top = setup.dataset.yCoord + 'px';
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+  var dialogHandle = setup.querySelector('.setup-user-pic');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
